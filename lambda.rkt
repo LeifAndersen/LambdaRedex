8387bd78f7dd8723887c3c3e3bb1415bc13a897e
Leif Andersen <leif@leifandersen.net>
2014-05-26 17:29:35 -0600

    Made extended language (doesn't catch errors properly).

--- lambda.rkt
+++ lambda.rkt
@@ -2,48 +2,56 @@
 
 (require redex)
 (require redex/tut-subst)
+(require pict)
 
 ; Based off of http://redex.racket-lang.org/lam-v.html
 
 (define-language λv
-  (e (e e ...) (if e e e) (if0 e e e) x v)
-  (v (λ (x ...) e) number + * = boolean)
-  (E (v ... E e ...) (if E e e) (if0 E e e) hole)
+  (e (e e ...)
+     (if e e e)
+     (if0 e e e)
+     lam x v)
+  (lam (λ (x ...) e) + * =)
+  (v number boolean)
   (x variable-not-otherwise-mentioned))
 
-;(define-extend-langauge λenv
-;  (a number)
-;  (ρ · (x : a))
-;  (σ · (a : v)))
+(define-extended-language λesk λv
+  (E (v ... E e ...)
+     (if E e e)
+     (if0 E e e)
+     hole)
+  (v .... lam closure)
+  (closure (clo e ρ))
+  (addr number)
+  (state (ς E ρ σ))
+  (ρ ((x addr) ...))
+  (σ ((addr val) ...)))
 
 (define red
   (reduction-relation
-   λv
-   (--> (in-hole E (+ number_1 ...))
-        (in-hole E ,(apply + (term (number_1 ...))))
+   λesk
+   (--> (in-hole E (+ number ...))
+        (in-hole E (Σ number ...))
         "+")
-   (--> (in-hole E (* number_1 ...))
-        (in-hole E ,(apply * (term (number_1 ...))))
+   (--> (in-hole E (* number ...))
+        (in-hole E (Π number ...))
         "*")
    (--> (in-hole E (= number_1 number_2))
-        (in-hole E ,(= (term number_1) (term number_2)))
+        (in-hole E (== number_1 number_2))
         "=")
    (--> (in-hole E (if0 0 e_1 e_2))
         (in-hole E e_1)
         "if0t")
-   (--> (in-hole E (if0 number_1 e_1 e_2))
+   (--> (in-hole E (if0 v e_1 e_2))
         (in-hole E e_2)
         "if0f"
-        (side-condition (not (= 0 (term number_1)))))
+        (side-condition (term (different v 0))))
    (--> (in-hole E (if #t e_1 e_2))
         (in-hole E e_1)
         "ift")
    (--> (in-hole E (if #f e_1 e_2))
         (in-hole E e_2)
         "iff")
-   (--> (in-hole E (if e_1 e_2 e_3))
-        (in-hole E (if #t e_2 e_3))
-        (side-condition (not (boolean? (term e_1)))))
    (--> (in-hole E ((λ (x ..._1) e) v ..._1))
         (in-hole E (subst (x v) ... e))
         "βv")
@@ -55,9 +63,6 @@
         (error "invalid application"))
    (--> (in-hole E (boolean any_1 ...))
         (error "invalid application"))
-   (--> (in-hole E (if0 any_1 any_2 any_3))
-        (error "invalid use of if0")
-        (side-condition (not (number? (term any_1)))))
    (--> (in-hole E (if any_1 any_2 any_3))
         (error "invalid use of if")
         (side-condition (and (not (boolean? (term any_1)))
@@ -78,9 +83,39 @@
                                 (length (term (v ...)))))))
    ))
 
-(define x? (redex-match λv x))
+(define-metafunction λesk
+  [(different v_1 v_1) #f]
+  [(different v_1 v_2) #t])
 
-(define-metafunction λv
+(define-metafunction λesk
+  Σ : number ... -> number
+  [(Σ number ...) ,(apply + (term (number ...)))])
+
+(define-metafunction λesk
+  Π : number ... -> number
+  [(Π number ...) ,(apply * (term (number ...)))])
+
+(define-metafunction λesk
+  == : number number -> boolean
+  [(== number_1 number_2) ,(= (term number_1) (term number_2))])
+
+(define-metafunction λesk
+  in-ρ : ρ x -> boolean
+  [(in-ρ ρ x) ,(not (equal? #f (assq (term x) (term ρ))))])
+
+(define-metafunction λesk
+  ρ-lookup : ρ x -> addr
+  [(ρ-lookup ρ x) ,(second (assq (term x) (term ρ)))])
+
+(define-metafunction λesk
+  σ-lookup : σ addr -> v
+  [(σ-lookup σ addr) ,(second (assq (term addr) (term σ)))])
+
+(define-metafunction λesk
+  ρσ-lookup : ρ σ x -> v
+  [(ρσ-lookup ρ σ x) (σ-lookup σ (ρ-lookup ρ x))])
+
+(define-metafunction λesk
   subst : (x v) ... e -> e
   [(subst (x v) ... e)
    ,(subst/proc x?
@@ -88,8 +123,9 @@
                 (term (v ...))
                 (term e))])
 
+(define x? (redex-match λesk x))
 
-(define value? (redex-match λv v))
+(define v? (redex-match λesk v))
 
 (define (single-step? e)
   (or (= (length (apply-reduction-relation red e))
@@ -102,23 +138,27 @@
 (define (progress-holds? e)
   (or (v? e) (reduces? e)))
 
-(define v? (redex-match λv v))
-
 (define (reduces? e)
   (not (null? (apply-reduction-relation red (term (,e))))))
 
 (test-->> red #:cycles-ok
-          (term ((λ (n) (n n)) (λ (n)  (n n)))))
+          (term ((λ (n) (n n)) (λ (n) (n n)))))
 (test-->> red #:cycles-ok
           (term ((λ (n) (if0 n 1 ((λ (x) (x x)) (λ (x) (x x))))) (+ 2 2))))
+(test-->> red
+          (term ((λ (x) (x x)) (λ (x) x)))
+          (term (λ (x) x)))
 (test-->> red
           (term ((λ (x) (if (= x 0) 5 9)) 0))
           (term 5))
 (test-->> red
           (term (if (= 0 0) 5 9))
           (term 5))
+(test-->> red
+          (term ((λ (x y) (+ x y)) 3 4))
+          (term 7))
 
-(redex-check λv e (or (value? (term e))
-                      (single-step? (term e))))
+(redex-check λesk e (or (v? (term e))
+                        (single-step? (term e))))
 
 (test-results)
